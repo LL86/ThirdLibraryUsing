6ea4d5cddd5a31980259f51d8915e3ed16138437
LL <18657141238@163.com>
2016-03-07 15:17:14 +0800

    project init

---
+++ ThirdLibraryUse/ThirdLibraryUse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThirdLibraryUse
+//
+//  Created by 史练练 on 16/3/7.
+//  Copyright © 2016年 史练练. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThirdLibraryUse/ThirdLibraryUse/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThirdLibraryUse
+//
+//  Created by 史练练 on 16/3/7.
+//  Copyright © 2016年 史练练. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
